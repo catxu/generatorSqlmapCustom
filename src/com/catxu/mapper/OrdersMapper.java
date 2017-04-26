b82cd6a2217101e9e0540ca27f5c786f57aab1b8
catxu <catxu@foxmail.com>
2017-04-26 14:01:53 +0800

    init

---
+++ src/com/catxu/mapper/OrdersMapper.java
@@ -0,0 +1,31 @@
+package com.catxu.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.catxu.pojo.Orders;
+import com.catxu.pojo.OrdersExample;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
